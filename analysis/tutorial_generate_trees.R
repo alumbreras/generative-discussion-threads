3df3ad67cbb54e6e0599d03425532c1b924fba82
Alberto Lumbreras <alberto.lumbreras@gmail.com>
2017-11-07 16:54:01 +0100

    fixed estimation. alpha needs still review

--- analysis/tutorial_generate_trees.R
+++ analysis/tutorial_generate_trees.R
@@ -2,6 +2,8 @@ alpha <- 100
 beta <- 0
 tau <- 1
 
+# Synthetic trees --------------------------------------------------------------
+
 # Generate a tree under Gomez model
 parents = gen.parentsvector.Gomez2013(n=10, alpha, beta, tau)
 plot.tree.nicely(parents_to_tree(parents))
@@ -16,7 +18,7 @@ df.trees <- all_parents_to_dataframe(parents)
 
 # Estimate its MLE parameters
 
-# Grid search for one parameter andthe others fixed
+# Grid search for one parameter and the others fixed
 x <- seq(80,10000, by=10)
 y = sapply(x, function(xx) likelihood_Gomez2013(df.trees, list(alpha=xx, beta=beta, tau=tau)))
 plot(x, y)
@@ -26,3 +28,8 @@ res <- estimation_Gomez2013(df.trees)
 cat("\n Estimated alpha:", res$alpha)
 cat("\n Estimated beta:", res$beta)
 cat("\n Estimated tau:", res$tau)
+
+
+# Real data --------------------------------------------------------------------
+# Load a Reddit dataset already either in the form of a df.tree or in the
+# form of a parents vector (maybe better, more universal)
